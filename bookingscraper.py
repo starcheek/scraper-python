3871c035f01f8fb16e399dabeaaf3f35b4c49313
starcheek <talfs00ezerins@gmail.com>
2021-06-10 17:38:51 +0300

    final push

--- bookingscraper.py
+++ bookingscraper.py
@@ -10,15 +10,14 @@ import re
 import pandas
 import xlsxwriter
 
-
 session = HTMLSession()
 
 LINK_DATE_FORMAT = "%Y-%m-%d"
 
 jolanta_results = {},
 bahnhofs_results = {}
-start_date = datetime.datetime(2021, 6, 10)
-end_date = datetime.datetime(2021, 6, 15)
+max_item_count = 0
+fetched_item_count = 0
 
 workbook = xlsxwriter.Workbook('results.xlsx')
 taken   = workbook.add_format({
@@ -28,20 +27,25 @@ taken   = workbook.add_format({
     'border_color': "#000000",
     'border': 1
 })
-available   = workbook.add_format({
+available = workbook.add_format({
     'bg_color': "#FFBFC3",
     'text_wrap': True,
     'valign': 'center',
     'border_color': "#000000",
     'border': 1
 })
+weekday = workbook.add_format({
+    'bg_color': "#caffbf",
+    'border_color': "#000000",
+    'border': 1
+})
 
 async def main():
-   # print("Date format: DD-MM-YYYY")
-    # start_date_input = input("Input start date:").split("-")
-    # end_date_input = input("Input end date:").split("-")
-    # start_date = datetime.datetime(int(start_date_input[2]), int(start_date_input[1]), int(start_date_input[0]))
-    # end_date = datetime.datetime(int(end_date_input[2]), int(end_date_input[1]), int(end_date_input[0]))
+    print("Date format: DD-MM-YYYY")
+    start_date_input = input("Input start date:").split("-")
+    end_date_input = input("Input end date:").split("-")
+    start_date = datetime.datetime(int(start_date_input[2]), int(start_date_input[1]), int(start_date_input[0]))
+    end_date = datetime.datetime(int(end_date_input[2]), int(end_date_input[1]), int(end_date_input[0]))
     tasks = []
     for hotel in baselinks:
         write_first_column(hotel)
@@ -61,15 +65,24 @@ def write_first_column(hotel):
             ws.write_string(index + 1, 0, room_pair[0])
             index += 1
 
+def print_percentage():
+    global fetched_item_count
+    global max_item_count
+    fetched_item_count += 1
+    print(str(int(fetched_item_count / max_item_count * 100)) + "%")  
+
 async def get_data(hotel_name, start_date, end_date):
     daterange = pandas.date_range(start_date, end_date)
-    
+    global max_item_count
+    max_item_count = len(daterange) * len(baselinks)
+
+    #Iterate over date range and get data from current day.
     for index, single_date in enumerate(daterange):
         next_day = single_date + datetime.timedelta(days=1)
         link = baselinks[hotel_name]
         link = re.sub(r'checkin=\d\d\d\d-\d\d-\d\d', "checkin=" + single_date.strftime(LINK_DATE_FORMAT), link)
-        link = re.sub(r'checkout=\d\d\d\d-\d\d-\d\d', "checkout=" + next_day.strftime(LINK_DATE_FORMAT), link)
-        await asyncio.gather(get_day_data(link, hotel_name, next_day, index+1), return_exceptions=False)
+        link = re.sub(r'checkout=\d\d\d\d-\d\d-\d\d', "checkout=" + next_day.strftime(LINK_DATE_FORMAT), link) # Replace date in link
+        await asyncio.gather(get_day_data(link, hotel_name, single_date, index+1), return_exceptions=False)
         
 async def get_day_data(link, hotel_name, date, column):
     page = session.get(link)
@@ -77,16 +90,17 @@ async def get_day_data(link, hotel_name, date, column):
     soup = BeautifulSoup(page.content, 'html.parser')
     table = soup.find('table', class_="hprt-table")
 
+    #If Cannt find table - all rooms are booked
     results = {}
     if table is not None:
         tbody = table.find('tbody')
         room_copy = {}
         comparison_room_id = -1
-        comparison_room_name = ""
+        comparison_room_name = "" # Used to mark previous items to group rooms
 
+        #Go trough all rows of table. 
         for tr in table.find_all('tr', class_="js-rt-block-row"):
             room_id = int(tr.find('select')["data-room-id"])
-            print(room_id)
             price = re.search(r'[0-9]{1,4}', tr.find('span', class_="prco-valign-middle-helper").get_text()).group()
             if room_id != comparison_room_id:
                 free_rooms = len(tr.find('select', {"data-room-id":str(room_id)}).find_all('option')) - 1
@@ -96,42 +110,45 @@ async def get_day_data(link, hotel_name, date, column):
             else:
                 room_copy[comparison_room_id][1][1].append(price)
 
+        #Create a map which shows how many rooms are not booked from all rooms
         for key in comparison_map.keys():
             if key in room_copy:
                 results[key] = (comparison_map[key][0], [comparison_map[key][1] - room_copy[key][1][0], room_copy[key][1][1]])
             else:
-                results[key] = (comparison_map[key][0], [comparison_map[key][1], []])
+                results[key] = (comparison_map[key][0], [0, []])
     else:
+        # If table is empty  - mark all rooms booked
         for key in comparison_map.keys():
             results[key] = (comparison_map[key][0], [0, []])
 
-    # print("\n\n\n\n\n")
+
 
 # ('Standarta divvietīgs numurs (1 gulta)', [2, ['55', '85']])
-
-
     ws = workbook.get_worksheet_by_name(hotel_name)
+
+    if date.weekday() in range(4, 7):
+        ws.write(0, column, date.strftime(LINK_DATE_FORMAT), weekday)
+    else:
+        ws.write(0, column, date.strftime(LINK_DATE_FORMAT))
     
-    ws.write(0, column, date.strftime(LINK_DATE_FORMAT))
     row = 1
     for room_key in results.keys():
         room = results[room_key]
-        counter = room[1][0]
+        available_room_count = room[1][0]
+
         for count in range(comparison_map[room_key][1]):
             price_string = ""
             for price in room[1][1]:
                 price_string += str(price) + "€/ "
 
-            if counter < 1:
+            if available_room_count < 1:
                 ws.write(row, column, "", taken)
             else:
                 ws.write(row, column, price_string, available)
-            count -= 1
+            
+            available_room_count -= 1
             row += 1
-           
-    # print(results)
- #  return results    
-
+    print_percentage()
 
 if __name__ == "__main__": 
     asyncio.run(main())
